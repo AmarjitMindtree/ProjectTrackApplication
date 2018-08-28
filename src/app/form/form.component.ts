601f7b97399325e7d9cca03526b442c3a017d2e2
Amarjit kumar <M1044368@mindtree.com>
2018-08-29 00:28:26 +0530

    My first commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -35,6 +35,7 @@ export class FormComponent implements OnInit {
   constructor(private myService: GetDataService) { }
 
   ngOnInit() {
+    console.log('Hello');
     this.myForm = new FormGroup({
       employeeID: new FormControl('', Validators.compose([ Validators.required, Validators.pattern('[M|m][1][0-9]{6}')])),
       projects: new FormControl(null,[Validators.required]),
